97352defc32ec31542d87beb7ab7e3e8663dd3f3
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-08-09 21:46:54 -0700

    slicing

---
+++ list_slicing.py
@@ -0,0 +1,14 @@
+my_string = "Hello, world!"
+# my_string[7:12] # from 7 to 12
+# 'world'
+print(my_string[7:12])
+
+print("-----")
+my_string = "Hello, world!"
+# my_string[:5] # from zero to 5
+print(my_string[:5])
+
+print("-----")
+# my_string[7:] # from 7 to the end
+print(my_string[7:])
+
