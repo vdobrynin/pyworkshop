d081d433be1ca851aefb77c52c8b29e9dedb1934
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-08-09 21:34:11 -0700

    comprehensions & comprehensions generator

--- list_comps.py
+++ list_comps.py
@@ -9,8 +9,42 @@ names = ["Nina", "Max", "Rose", "Jimmy"]
 my_list = [len(name) for name in names]
 print("Before:", my_list)
 
+print("-------")
 # my_list = [len(name) for name in names if len(name) % 2 == 0]
 my_list = [len(name) for name in names if len(name) % 2 != 0]
 print("After:", my_list)
 
+print("-------")
+scores = {f"player-{num}":0 for num in range(0, 5)}
+print(scores)
 
+print("-------")
+my_list = [(f"player-{num}", num * 2) for num in range(0, 5)]
+print(my_list)
+
+print("-------")
+scores = {key:value for (key, value) in my_list}
+print(scores)
+
+print("-------")
+my_set = {num for num in [1, 2, 1, 0, 3]}
+print(my_set)
+# {0, 1, 2, 3}
+
+print("-------")
+list_comp = [x ** 2 for x in range(10) if x % 2 == 0]
+print(list_comp)
+# [0, 4, 16, 36, 64]
+
+print("-------")
+gen_exp = (x ** 2 for x in range(10) if x % 2 == 0)
+print(gen_exp)
+for num in gen_exp:
+    print(num)
+
+print("-------")
+# gen_exp = (x ** 2 for x in range(10) if x % 2 == 0)
+# for num in gen_exp:
+#     print(num)
+# for num in gen_exp:
+#     print(num)   
--- split_join.py
+++ split_join.py
@@ -1,3 +1,26 @@
+# my_data1 = "this,is,comma,separated,data"
+# my_data1 = my_data1.split(",")
+# print(my_data1)
+
+# my_data2 = "this,is,comma,separated,data"     
+# ":".join(my_data2)
+# print(my_data2)
+
+# my_data3 = "this,is,comma,separated,data"
+# ", ".join(my_data3)
+# print(my_data3)
+
 my_data = "this,is,comma,separated,data"
 my_data = my_data.split(",")
-print(my_data
+print(my_data)
+
+# ":".join(my_data)
+# # print(my_data)
+
+# ", ".join(my_data)
+# # print(my_data)
+
+squares = {num:num * num for num in range(10)}
+print(squares)
+# {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
+
