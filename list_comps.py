a4aab5d37e4a4d27edbf82fc040b319671c7a9ea
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-08-09 19:10:06 -0700

    split_join

--- list_comps.py
+++ list_comps.py
@@ -1,6 +1,16 @@
+# names = ["Nina", "Max", "Rose", "Jimmy"]
+# my_list = [] # empty list
+# for name in names:
+#     my_list.append(len(name))
+# print(my_list)
+# [4, 3, 4, 5]
+
 names = ["Nina", "Max", "Rose", "Jimmy"]
-my_list = [] # empty list
-for name in names:
-    my_list.append(len(name))
-print(my_list)
-[4, 3, 4, 5]
+my_list = [len(name) for name in names]
+print("Before:", my_list)
+
+# my_list = [len(name) for name in names if len(name) % 2 == 0]
+my_list = [len(name) for name in names if len(name) % 2 != 0]
+print("After:", my_list)
+
+
---
+++ split_join.py
@@ -0,0 +1,3 @@
+my_data = "this,is,comma,separated,data"
+my_data = my_data.split(",")
+print(my_data
