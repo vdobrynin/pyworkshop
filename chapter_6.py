0f276d828c6f0ffed817505ecbd33506ca9709d9
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-08-15 23:56:25 -0700

    inheritance 2

--- chapter_6.py
+++ chapter_6.py
@@ -55,18 +55,38 @@
 # # But, when we call our class method on our instance:
 # print(f"My car has {my_car.get_number_of_wheels()} wheels.")
 
-class Car:
-    runs = True
+# class Car:
+#     runs = True
 
+#     def __init__(self, make, model):
+#         self.make = make
+#         self.model = model
+
+#     def start(self):
+#         if self.runs:
+#             print(f"Your {self.make} {self.model} is started. Vroom vroom!")
+#         else:
+#             print(f"Your {self.make} {self.model} is broken :(")
+
+# my_car = Car("Ford", "Thunderbird")
+# my_car.start()
+
+class Car:
     def __init__(self, make, model):
         self.make = make
         self.model = model
 
-    def start(self):
-        if self.runs:
-            print(f"Your {self.make} {self.model} is started. Vroom vroom!")
-        else:
-            print(f"Your {self.make} {self.model} is broken :(")
+    def __str__(self):
+        return f"<<Car object: {self.make} {self.model}>>"
+
+    def __repr__(self):
+        return f"Car('{self.make}', '{self.model}')"
 
 my_car = Car("Ford", "Thunderbird")
-my_car.start()
+print(f"This object is a {str(my_car)}")
+print(f"To reproduce it, type: {repr(my_car)}")
+
+# my_ints = [int(str_num) for str_num in ["1", "2", "3"]]
+# print(my_ints)
+# # [1, 2, 3]
+
--- vehicle.py
+++ vehicle.py
@@ -12,3 +12,16 @@ class Car(Vehicle):
 
     def __init__(self, make, model, fuel="gas"):
         super().__init__(make, model, fuel)
+
+# chapter_6.py
+
+from vehicle import Vehicle, Car
+
+my_car = Car("Ford", "Thunderbird")
+print(f"my_car is type {type(my_car)}")
+print(f"my_car uses {my_car.fuel}")
+
+print(f"my_car is a Car: {isinstance(my_car, Car)}")
+print(f"my_car is a Vehicle: {isinstance(my_car, Vehicle)}")
+print(f"Car is a subclass of Vehicle: {issubclass(Car, Vehicle)}")
+
